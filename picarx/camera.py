f21ea7c5aedaa2fa8650c4183a1fae2ef4164766
nnamd <119299028+nwaggu@users.noreply.github.com>
2023-02-03 07:16:25 -0800

    Update camera.py

--- picarx/camera.py
+++ picarx/camera.py
@@ -99,7 +99,10 @@ class CameraController(object):
         #Setup car to follow line
         self.car = px.Picarx()
         self.car.set_camera_servo1_angle(0)
+        time.sleep(0.5)
         self.car.set_camera_servo1_angle(30)
+        time.sleep(0.5)
+        self.car.set_dir_servo_angle(0)
     
     def steer(self, scaling):
         self.scaling = scaling*2
